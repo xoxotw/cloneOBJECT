ddaaf803b593f0f9ec0981b28c2f85e9d23268f6
Peter Silver <xoxo@crystalise.tw>
2013-08-29 18:52:52 +0200

    final

---
+++ cloneOBJ/cloneOBJ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cloneOBJ
+{
+    public class Program
+    {
+              
+        static void Main(string[] args)
+        {
+            var myCar = new Vehicle("VOLVO", "BLUE", 1988);
+            var my2car = new Vehicle("PAGANI HYURA", "PINK", 2013);
+            
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("myCar is a: {1} {0} from year: {2}" + Environment.NewLine, myCar.Brand, myCar.Color, myCar.Year);
+                        
+            myCar.CloneCar(myCar);
+            myCar.CloneCar(my2car);
+            
+            Console.ReadLine();
+
+        }
+               
+    }
+}
---
+++ cloneOBJ/cloneOBJ/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cloneOBJ
+{
+    public class Vehicle
+    {
+        public string Brand;
+        public string Color;
+        public int Year;
+
+        public Vehicle()
+        { 
+            
+        }
+
+        public Vehicle(string brand, string color, int year)
+        {
+            Brand = brand;
+            Color = color;
+            Year = year;
+        }
+
+        public void CloneCar(Vehicle mycar)
+        {
+            var myClonedCar = new Vehicle();
+            myClonedCar = mycar;
+            if (myClonedCar.Color == "BLUE")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            else
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("myClonedCar is a: {1} {0} from year: {2}" + Environment.NewLine, myClonedCar.Brand, myClonedCar.Color, myClonedCar.Year);
+        }
+    }
+}
